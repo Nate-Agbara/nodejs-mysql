b9423a3640c15cda8fb83cfacd4a1e59962dc62f
Nathan Agbara <agbaranath@gmail.com>
2023-02-09 10:35:02 +0100

    implemented swagger doucumentation for api

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const expressWinston = require('express-winston');
 const {transports, format} = require('winston');
 
 const logger = require('./logger');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const options = require('./swagger');
 
 const app = express(); 
 const PORT = process.env.SERVER_PORT || 3000;
@@ -40,12 +43,67 @@ app.use(expressWinston.errorLogger({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: API endpoints to manage users
+ * 
+ */
+
+/**
+ * @swagger
+ *   /users:
+ *     get:
+ *       summary: Get all users
+ *       tags: [Users]
+ *       responses:
+ *         "200":
+ *           description: The list of users
+ *           contents:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *         "400":
+ *           $ref: '#/components/responses/400'
+ *         "401":
+ *           $ref: '#/components/responses/401'
+ * 
+ */
 app.get('/users', (req, res) => {
     knex.select().from('users').then(function(users){
         res.send(users)
     });
 });
 
+/**
+ * @swagger
+ *   /users/{id}:
+ *     get:
+ *       summary: Get a user by id
+ *       tags: [Users]
+ *       parameters: 
+ *         - in: path
+ *           name: id
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: id of a book
+ *       responses:
+ *         "200":
+ *           description: The list of users
+ *           contents:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *         "400":
+ *           $ref: '#/components/responses/400'
+ *         "401":
+ *           $ref: '#/components/responses/401'
+ *         "404":
+ *           $ref: '#/components/responses/404'
+ * 
+ */
 app.get('/users/:id', (req, res) => {
     knex.select()
         .from('users')
@@ -57,6 +115,30 @@ app.get('/users/:id', (req, res) => {
     logger.warn('This is a warn log!');
 });
 
+
+/**
+ * @swagger
+ *   /users:
+ *     post:
+ *       summary: Create a user
+ *       tags: [Users]
+ *       requestBody:
+ *           required: true
+ *           content: 
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *       responses:
+ *         "400":
+ *           $ref: '#/components/responses/400'
+ *         "401":
+ *           $ref: '#/components/responses/401'
+ *         "201":
+ *           description: User created successfully
+ *           contents: 
+ *             application/json
+ * 
+ */
 app.post('/users', (req, res) => {
     knex('users').insert({
         name: req.body.name,
@@ -69,7 +151,42 @@ app.post('/users', (req, res) => {
     });
 });
 
-app.put('/users/:id', (req, res) => {
+/**
+ * @swagger
+ *   /users/{id}:
+ *     patch:
+ *       summary: Get a user by id
+ *       tags: [Users]
+ *       parameters: 
+ *         - in: path
+ *           name: id
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: id of a book
+ *       requestBody:
+ *           required: true
+ *           content: 
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *                 required:
+ *       responses:
+ *         "204":
+ *           description: User updated successfully
+ *           contents:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *         "400":
+ *           $ref: '#/components/responses/400'
+ *         "401":
+ *           $ref: '#/components/responses/401'
+ *         "404":
+ *           $ref: '#/components/responses/404'
+ * 
+ */
+app.patch('/users/:id', (req, res) => {
     knex('users').update({
         name: req.body.name,
         email: req.body.email
@@ -82,6 +199,34 @@ app.put('/users/:id', (req, res) => {
     });
 });
 
+/**
+ * @swagger
+ *   /users/{id}:
+ *     delete:
+ *       summary: Delete a user by id
+ *       tags: [Users]
+ *       parameters: 
+ *         - in: path
+ *           name: id
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: id of a book
+ *       responses:
+ *         "200":
+ *           description: User deleted successfully
+ *           contents:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *         "400":
+ *           $ref: '#/components/responses/400'
+ *         "401":
+ *           $ref: '#/components/responses/401'
+ *         "404":
+ *           $ref: '#/components/responses/404'
+ * 
+ */
 app.delete('/users/:id', (req, res) => {
     knex('users')
     .del()
@@ -101,6 +246,10 @@ app.get('/', (req, res) =>
     res.send(`Node and express server is running on port ${PORT}`) );
 
 app.use(errorHandler)
+
+//swagger setup
+const specs = swaggerJSDoc(options)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
 
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`) );
---
+++ swagger.js
@@ -0,0 +1,65 @@
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'CRUD api',
+            version: '1.0.0',
+            description: 'Simple CRUD api with MySQL'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3001',
+                description: 'development server'
+            }
+        ],
+        components: {
+            schemas: {
+                User: {
+                    type: 'object',
+                    required: ['name', 'email'],
+                    properties: {
+                        name: {
+                            type: 'string',
+                            description: 'name of the user'
+                        },
+                        email: {
+                            type: 'string',
+                            description: 'email of the user'
+                        }
+                    },
+                    example: {
+                        name: 'test name',
+                        email: 'test@yopmail.com'
+                    }
+                }
+            },
+            responses: {
+                400: {
+                    description: 'Missing api key - include it in the authorization header',
+                    contents: 'application/json'
+                },
+                401: {
+                    description: 'Unauthorized - incorrect api key or incorrect format',
+                    contents: 'application/json'
+                },
+                404: {
+                    description: 'Not found - no user found',
+                    contents: 'application/json'
+                }
+            },
+            securitySchemes: {
+                ApiKeyAuth: {
+                    type: 'apiKey',
+                    in: 'header',
+                    name: 'Authorization'
+                }
+            }
+        },
+        security: [{
+            ApiKeyAuth: []
+        }]
+    },
+    apis: ['./index.js']
+}
+
+module.exports = options
